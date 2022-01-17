194de434db4c870288349ac43f5080bdb8fe7486
eogus0512 <eogus0512@gmail.com>
2022-01-17 22:11:24 +0900

    dp 문제 추가

--- baekjoon/DP/10844.java
+++ baekjoon/DP/10844.java
@@ -12,15 +12,15 @@ public class Main {
         long sum=0;
         dp = new long [N+1][10];
 
-        for(int i=1;i<=9;i++) { // dp배열 초기상태
+        for(int i=1;i<=9;i++) {
             dp[1][i] = 1;
         }
 
-        for(int i=2;i<=N;i++) { // 2인 경우부터 N까지 반복
+        for(int i=2;i<=N;i++) {
             for(int j=0;j<10;j++) {
-                if(j==0) dp[i][j] = (dp[i-1][j+1]) % MOD; // 끝자리가 0일 경우는 1일 때만 고려
-                else if(j==9) dp[i][j] = dp[i-1][j-1] % MOD ; // 끝자리가 9일 경우는 8일 때만 고려
-                else dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])% MOD; // 나머지는 -1, +1 모두 고려
+                if(j==0) dp[i][j] = (dp[i-1][j+1]) % MOD;
+                else if(j==9) dp[i][j] = dp[i-1][j-1] % MOD ;
+                else dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])% MOD;
             }
         }
 
