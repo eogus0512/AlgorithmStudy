aa6d176605e33fa68699417755d7e44f1c97af59
eogus0512 <eogus0512@gmail.com>
2023-07-23 16:18:48 +0900

    Create 2589 (보물섬).py

---
+++ Daehyun/2589 (보물섬).py
@@ -0,0 +1,35 @@
+from collections import deque
+
+n, m=map(int, input().split())
+map=[list(input()) for _ in range(n)]
+
+dx=[0, 0, 1, -1]
+dy=[1, -1, 0, 0]
+
+def bfs(i, j):
+    queue=deque()
+    queue.append((i,j))
+    visited=[[0]*m for _ in range(n)]
+    visited[i][j]=1
+    cnt=0
+
+    while queue:
+        x, y=queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx>=0 and nx < n and ny >= 0 and ny < m:
+                if map[nx][ny]=='L' and visited[nx][ny]==0:
+                    visited[nx][ny]=visited[x][y]+1
+                    cnt=max(cnt, visited[nx][ny])
+                    queue.append((nx, ny))
+    return cnt-1
+
+result=0
+
+for i in range(n):
+  for j in range(m):
+    if map[i][j]=='L':
+      result = max(result, bfs(i,j))
+
+print(result)
